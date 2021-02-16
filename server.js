b52aefde0315f2ae40591de50efc15c600651b69
Alheli Miranda <alhealhe72@hotmail.com>
2021-02-15 19:40:06 -0500

    trying to deployed to heroku

--- server.js
+++ server.js
@@ -14,10 +14,20 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/wokout", {
+/* mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/wokout", {
   useNewUrlParser: true,
   useFindAndModify: false
-});
+}); */
+
+mongoose.connect(
+  process.env.MONGODB_URI || 'mongodb://localhost/workout',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  }
+);
 
 // routes
 app.use(require('./routes/api-routes'));
